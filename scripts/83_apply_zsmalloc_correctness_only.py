cb1b62600c6cbce4078606af05beb551c46c8064
Giuliano Basso <67099534+GiulianoB-1@users.noreply.github.com>
2026-09-13 13:56:22 +0300

    Phase 83: emit real zsmalloc barrier whitespace

--- scripts/83_apply_zsmalloc_correctness_only.py
+++ scripts/83_apply_zsmalloc_correctness_only.py
@@ -18,7 +18,7 @@ changes = []
 # b19675b6a046da6b4e160a78eedb3dd2584aa889.
 needle = """	atomic_long_dec(&pool->isolated_pages);
 """
-marker = "\\tsmp_mb__after_atomic();\\n"
+marker = chr(9) + "smp_mb__after_atomic();" + chr(10)
 fn = "static inline void zs_pool_dec_isolated(struct zs_pool *pool)"
 start = s.find(fn)
 if start < 0:

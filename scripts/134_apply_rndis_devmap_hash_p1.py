e1c4773fd21af84258daa105414a0a9c8cb71fb8
Giuliano Basso <67099534+GiulianoB-1@users.noreply.github.com>
2026-09-22 17:56:38 +0300

    usb: adapt DEVMAP_HASH P1 to Samsung devmap layout

--- scripts/134_apply_rndis_devmap_hash_p1.py
+++ scripts/134_apply_rndis_devmap_hash_p1.py
@@ -47,7 +47,6 @@ ops_anchor = """const struct bpf_map_ops dev_map_hash_ops = {
 	.map_lookup_elem = dev_map_hash_lookup_elem,
 	.map_update_elem = dev_map_hash_update_elem,
 	.map_delete_elem = dev_map_hash_delete_elem,
-	.map_check_btf = map_check_no_btf,
 };
 
 static int dev_map_notification(struct notifier_block *notifier,
